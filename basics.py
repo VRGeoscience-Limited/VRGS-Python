cd32b3cc97479cadd116912800b7a3327669349e
Brian Burnham <164886005+vrgeobsb@users.noreply.github.com>
2024-06-19 15:21:23 +0100

    Update to basics.py
    Added python methods in VRGS

--- basics.py
+++ basics.py
@@ -1,1 +1,32 @@
-# TODO: Add python commands
+# VRGS Python methods
+import vrgs
+
+# Return the number list of meshes in the project
+vrgs.mesh_list()
+
+# Return the list of point clouds in the project
+vrgs.pointcloud_list()
+
+# Return the list of vetices in a mesh_list
+vrgs.meshvertices()
+
+# Return the list of vertices in a point cloud
+vrgs.pclvertices()
+
+# Return the number of attributes of a mesh
+vrgs.meshattributelist()
+
+# Return the number of the attributes of a point cloud 
+vrgs.pclattributelist()
+
+# Return the attributes of a mesh
+vrgs.meshattribute()
+
+# Return the attributes of a point cloud 
+vrgs.pclattribute()
+
+# Add attributes to a mesh after computation
+vrgs.addmeshattribute()
+
+# Return a list of all orientations in the project
+vrgs.orientations()
